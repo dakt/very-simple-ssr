961749e0ed0c7084d9ccf416f574a1ccae995cc0
Darijo Kutlesa <darijo.kutlesa@moj-posao.net>
2018-03-27 09:47:56 +0200

    Home refactored

---
+++ src/shared/Home/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import List from './List';
+
+
+export class GistList extends React.Component {
+
+    static async getInitialData({ isServer, dispatch, getState }) {
+        const {
+            route: { qs },
+            gist: { pagination },
+        } = getState();
+
+        const page  = qs.page ? qs.page : pagination.page;
+        const limit = qs.limit ? qs.limit : pagination.limit;
+        const url   = `https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${limit}`;
+
+        try {
+            dispatch({ type: 'GET_DATA_REQUEST' });
+
+            const response = await fetch(url);
+            const count = response.headers.get('x-total-count');
+            const payload = { 
+                data: await response.json(),
+                pagination: { page, limit, count },
+            };
+
+            dispatch({ type: 'GET_DATA_SUCCESS', payload });
+        } catch (error) {
+            dispatch({ type: 'GET_DATA_FAILURE', error });
+        }
+    }
+
+    render() {
+        return (
+            <List 
+                data={this.props.data}
+                loading={this.props.loading}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    loading: state.gist.loading,
+    data: state.gist.data,
+    pagination: state.gist.pagination,
+});
+
+export default connect(mapStateToProps)(GistList);
--- src/shared/Home/List.js
+++ src/shared/Home/List.js
@@ -1,41 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import UserCard from './UserCard';
 
 
-export class GistList extends React.Component {
+export default class List extends React.Component {
 
     mouseDown = false;
     lastMouseX = null;
     positionX = null;
-    cards = [];
-
-    static async getInitialData({ isServer, dispatch, getState }) {
-        const {
-            route: { qs },
-            gist: { pagination },
-        } = getState();
-
-        const page  = qs.page ? qs.page : pagination.page;
-        const limit = qs.limit ? qs.limit : pagination.limit;
-        const url   = `https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${limit}`;
-
-        try {
-            dispatch({ type: 'GET_DATA_REQUEST' });
-
-            const response = await fetch(url);
-            const count = response.headers.get('x-total-count');
-            const payload = { 
-                data: await response.json(),
-                pagination: { page, limit, count },
-            };
-
-            dispatch({ type: 'GET_DATA_SUCCESS', payload });
-        } catch (error) {
-            dispatch({ type: 'GET_DATA_FAILURE', error });
-        }
-    }
 
     update(element, clientX) {
         // Calculate delta
@@ -110,13 +82,9 @@ export class GistList extends React.Component {
     }
 
     render() {
-        const { data, pagination: { page, limit, count } } = this.props;
-
-        const totalPages = (count / limit);
-
         return (
             <div style={{ overflowX: "hidden" }}>
-                {data.map(user => (
+                {this.props.data.map(user => (
                     <UserCard
                         key={user.id}
                         data={user}
@@ -133,11 +101,3 @@ export class GistList extends React.Component {
         );
     }
 }
-
-const mapStateToProps = (state) => ({
-    loading: state.gist.loading,
-    data: state.gist.data,
-    pagination: state.gist.pagination,
-});
-
-export default connect(mapStateToProps)(GistList);
