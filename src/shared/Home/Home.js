1dfefc909d900f407452d669fb84d87603e2bc28
Darijo Kutlesa <darijo.kutlesa@moj-posao.net>
2018-03-28 12:01:05 +0200

    Reallocated common components

--- src/shared/Home/Home.js
+++ src/shared/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import List from './List';
+import List from '../components/List';
 import UserCard from './UserCard';
 import styles from './Home.css';
 
--- src/shared/Home/UserCard.js
+++ src/shared/Home/UserCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 
-import Check from './Check';
+import Check from '../components/Check';
 import styles from './UserCard.css';
 
 
