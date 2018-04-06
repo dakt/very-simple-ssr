634e0b18f3baa745a1d32b9a70cf057e36872f21
Darijo Kutlesa <darijo.kutlesa@moj-posao.net>
2018-04-06 11:05:47 +0200

    Added burger and back button

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import styles from './app.css';
 import './global.css';
 
 
-export default ({ children }) => (
+export default withRouter(({ children, location }) => (
     <div className={styles.app}>
         <div>
             <div className={styles.navigationBar}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
+                <Link to="/">
+                    { location.pathname === '/' 
+                        ? (<i className="material-icons">menu</i>)
+                        : (<i className="material-icons">keyboard_arrow_left</i>)
+                    }
+                </Link>
             </div>
             <div className={styles.body}>
                 { children }
             </div>
         </div>
     </div>
-);
+));
