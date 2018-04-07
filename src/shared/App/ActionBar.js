0fde68d1734bfbae5979d6a2897e6345695a45e3
Darijo <darijocgi@gmail.com>
2018-04-07 22:47:09 +0200

    Delete icon animation

--- src/shared/App/ActionBar.js
+++ src/shared/App/ActionBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Selectors } from './redux';
+import { CSSTransition } from 'react-transition-group';
 
 import Icon from '../components/Icon';
+import { Selectors } from './redux';
+
+import './ActionBar.css';
 
 
 const ActionBar = ({ visible }) => (
     <div>
-        { visible && <Icon name="delete" onClick={() => alert('!')} /> }
+        <CSSTransition in={visible} classNames="actionBar" timeout={{ enter: 200, exit: 200 }}>
+            <div className="actionBar">
+                <Icon name="delete" onClick={() => alert('!')} />
+            </div>
+        </CSSTransition>
     </div>
 );
 
